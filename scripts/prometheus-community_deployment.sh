5f9aaa2c5620a3016c12e3b05d45bbe5136443b7
Durgesh Kumar Meena <58581435+durgeshmeena@users.noreply.github.com>
2025-08-28 02:12:18 +0530

    k8s: add monitoring stack

---
+++ scripts/prometheus-community_deployment.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# check if grafana admin password is set
+if [[ -z "$GRAFANA_ADMIN_PASSWORD" ]]; then
+  echo "GRAFANA_ADMIN_PASSWORD environment variable must be set"
+  exit 1
+fi
+
+# add prometheus-community helm repo
+helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
+
+# update helm repos
+helm repo update
+
+# install monitoring stack(prometheus,grafana,kube-state-metrics) prometheus and create Service Monitor and Pod Monitor for book-store
+helm upgrade --install kube-prometheus-stack prometheus-community/kube-prometheus-stack \
+    --namespace monitoring --create-namespace \
+    -f ./deployment/monitoring/kube-prometheus-stack-helm-values.yaml \
+    -f ./deployment/monitoring/book-store-monitoring-helm-values.yaml
+
+# create configMap containgin grafana dashboard for book-store
+kubectl apply -f ./deployment/monitoring/book-store-grafana-dashboard-config.yaml
